8df174e42dc547aae5c424bef8acebeb95187f9a
Viktoria <airotkiv15@gmail.com>
2023-02-24 23:24:38 -0800

    Cleaned up method that creates next piece panel. Does not display the next tetris piece still.

--- src/viewer/NextPiece.java
+++ src/viewer/NextPiece.java
@@ -1,12 +1,24 @@
 package viewer;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+/**
+ * This class creates a panel to displays the next tetris piece to the user
+ * on the top right of the game.
+ *
+ * @author Viktoria Dolojan
+ * @version 1.1
+ */
 public class NextPiece extends JPanel {
 
-    /** Width and height of next piece panel. */
-    private static final int SIZE = 150;
+    /** Width and height of panel. */
+    private static final int SIZE = 250;
+
+    /** Panel that displays the next tetris piece. */
+    private static JPanel myNextPiece;
 
     /*
      * Creates the panel that displays the next tetris piece
@@ -14,12 +26,25 @@ public class NextPiece extends JPanel {
      * @param theY y coordinate of panel
      * @param theSize size of square panel
      */
-    public static void createPiecePanel(final int theX, final int theY) {
-        // creates a panel with blue background
-        JPanel npFrame = new JPanel();
-        npFrame.setBackground(Color.blue);
-        npFrame.setBounds(theX, theY, SIZE, SIZE);
+    private static void createPiecePanel() {
+        myNextPiece = new JPanel();
+        myNextPiece.setBackground(Color.BLUE);
+        myNextPiece.setBounds(SIZE, 0, SIZE, SIZE);
+        // TODO: display next tetris piece inside panel
+    }
 
-        // figure out how to display next tetris piece inside panel
+    // TODO: add panel to frame
+
+    // main for testing
+    public static void main(final String[] theArgs) {
+        final JFrame frame = new JFrame();
+        final int width = 500;
+        final int height = 1000;
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(new Dimension(width, height));
+        frame.setVisible(true);
+        createPiecePanel();
+        frame.add(myNextPiece);
     }
 }
