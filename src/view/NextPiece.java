c50c14cd1a0eb15f6f2f1cb43f7b00be98a15fa5
Viktoria <airotkiv15@gmail.com>
2023-03-03 09:37:55 -0800

    Refined code

---
+++ src/view/NextPiece.java
@@ -0,0 +1,32 @@
+/*
+ * TCSS 305 - Tetris Project
+ *
+ */
+
+package view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+
+/**
+ * This class creates a panel to displays the next tetris piece to the user
+ * on the top right of the game.
+ *
+ * @author Viktoria Dolojan
+ * @version Winter 2023
+ */
+public class NextPiece extends JPanel {
+
+    /** Width and height of the panel. */
+    private static final int PANEL_SIZE = 200;
+
+    /** Constructor for NextPiece panel. */
+    public NextPiece() {
+        super();
+        setBackground(Color.BLUE);
+        setPreferredSize(new Dimension(PANEL_SIZE, PANEL_SIZE));
+        setBorder(new TitledBorder("Next Tetris Piece"));
+    }
+}
